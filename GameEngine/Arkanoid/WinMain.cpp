ec1322c9afa8efd6ecc03f22467ec7f8419bb870
Divinecrusade <gorlovdima@inbox.ru>
2024-01-21 13:09:46 +0300

    feat: Add empty new project for Arkanoid game

---
+++ GameEngine/Arkanoid/WinMain.cpp
@@ -0,0 +1,17 @@
+#include "../Game.hpp"
+#include "../MainWindow/MainWindow.hpp"
+#include "../GraphicsDirect2D.hpp"
+
+
+using namespace GameEngine2D;
+
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+{
+    MainWindow& m_wnd{ MainWindow::instance(hInstance, nCmdShow, L"Arkanoid") };
+    GraphicsDirect2D graphics{ m_wnd.get_window_handler() };
+    Game theGame{ m_wnd, graphics };
+    theGame.start();
+
+    return EXIT_SUCCESS;
+}
--- GameEngine/Game.cpp
+++ GameEngine/Game.cpp
@@ -31,12 +31,5 @@ namespace GameEngine2D
 
     void Game::render()
     {
-        auto const w{ gfx.get_screen_width() };
-        auto const h{ gfx.get_screen_height() };
-
-        gfx.draw_rectangle(0, 0, 200, 300, 255, 0, 0); // red
-        gfx.draw_rectangle(w - 200, h - 200, w, h, 255U, 0U, 0U); // red
-        gfx.draw_line(0, 0, w, h, 3U, 0U, 255U, 0U); // green
-        gfx.fill_rectangle(0, 0, 200, 300, 0U, 0U, 255U); // blue
     }
 }
