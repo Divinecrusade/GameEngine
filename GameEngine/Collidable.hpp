2e9a63eb6f8db5f929e5a5469b9d2069e7d23a3c
Divinecrusade <gorlovdima@inbox.ru>
2024-08-16 14:13:36 +0300

    refactor: Delete useless maybe_unused attribute

--- GameEngine/Collidable.hpp
+++ GameEngine/Collidable.hpp
@@ -20,11 +20,11 @@ namespace GameEngine::Abstract
 
         virtual Geometry::Rectangle2D<int> get_collision_box() const = 0;
 
-        [[maybe_unused]] bool is_collided_with(Collidable const& obj) const noexcept
+        bool is_collided_with(Collidable const& obj) const noexcept
         {
             return this->get_collision_box().is_colided_with(obj.get_collision_box());
         }
-        [[maybe_unused]] int get_sqr_distance(Collidable const& obj)  const noexcept
+        int get_sqr_distance(Collidable const& obj)  const noexcept
         {
             return (this->get_collision_box().get_center() - obj.get_collision_box().get_center()).get_square_length();
         }
