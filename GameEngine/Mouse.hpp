7d8f5159432db033034f8620d3b8ed68e5158e9d
Divinecrusade <gorlovdima@inbox.ru>
2024-09-10 12:19:46 +0300

    feat: Implement cursor area getter

--- GameEngine/Mouse.hpp
+++ GameEngine/Mouse.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "Vector2D.hpp"
+#include "Rectangle2D.hpp"
 #include "IWindow.hpp"
 #include "WinApiException.hpp"
 
@@ -35,6 +35,28 @@ namespace GameEngine
             if (!ScreenToClient(wnd.get_window_handler(), &cursor_pos)) throw std::runtime_error{ "Failed to convert global mouse position to window local position" };
 
             return { cursor_pos.x, cursor_pos.y };
+        }
+
+        static Geometry::Rectangle2D<int> get_cursor_area()
+        {
+            CURSORINFO cursor_info{ };
+            cursor_info.cbSize = sizeof(CURSORINFO);
+            SIZE       cursor_size{ };
+            
+            if (HCURSOR const cursor_handler{ GetCursor() }; cursor_handler == NULL) throw std::runtime_error{ "Failed to get cursor info, because there is no cursor" };
+            else if (!GetCursorInfo(&cursor_info)) throw WinApiException{ "Failed to get cursor info" };
+            else if (ICONINFO icon_info{ }; !GetIconInfo((HICON)cursor_handler, &icon_info)) throw WinApiException{ "Failed to get icon info" };
+            else if (BITMAP   icon_bitmap{ }; GetObjectW(icon_info.hbmMask, sizeof(icon_bitmap), &icon_bitmap) != sizeof(icon_bitmap)) throw WinApiException{ "Failed to get bitmap info" };
+            else
+            {
+                cursor_size.cx = icon_bitmap.bmWidth;
+                cursor_size.cy = icon_info.hbmColor ? icon_bitmap.bmHeight : icon_bitmap.bmHeight / 2;
+
+                if (!DeleteObject(icon_info.hbmMask))  throw std::runtime_error{ "Failed to delete bmMask object" };
+                if (icon_info.hbmColor != NULL && !DeleteObject(icon_info.hbmColor)) throw std::runtime_error{ "Failed to delete bmColor object" };
+            }
+
+            return Geometry::Rectangle2D<int>{ Geometry::Vector2D<int>{ cursor_info.ptScreenPos.x, cursor_info.ptScreenPos.y }, cursor_size.cx, cursor_size.cy };
         }
 
     private:
